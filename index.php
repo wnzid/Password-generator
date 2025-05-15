cafb8c4b045a9bcf168f19739dc8c5d7300f4fea
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-05-15 17:42:36 +0300

    v4.1

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-header("Location: signup.php");
-exit();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Manager</title>
+</head>
+<body>
+    <h2>Welcome to the Password Manager</h2>
+    <p>Please choose an option:</p>
+
+    <ul>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</body>
+</html>
