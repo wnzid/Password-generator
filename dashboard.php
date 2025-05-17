931af4ff79aafdafaf3a7fe156c6bd3fbe05fed5
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-05-17 11:58:09 +0300

    v4.2

--- dashboard.php
+++ dashboard.php
@@ -5,16 +5,50 @@ if (!isset($_SESSION['user'])) {
     header("Location: login.php");
     exit();
 }
+
+$username=htmlspecialchars($_SESSION['user']);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Dashboard</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .dashboard {
+            width: 60%;
+            margin: auto;
+            padding-top: 50px;
+        }
+        h2 {
+            color: #333;
+        }
+        ul {
+            list-style-type: none;
+            padding-left: 0;
+        }
+        li {
+            margin-bottom: 10px;
+        }
+        a {
+            text-decoration: none;
+            color: blue;
+        }
+    </style>
 </head>
 <body>
-    <h2>Welcome, <?php echo htmlspecialchars($_SESSION['user']); ?>!</h2>
-    <p>You are logged in.</p>
-    <a href="logout.php">Logout</a>
+    <div class="dashboard">
+        <h2>Welcome, <?php echo $username; ?>!</h2>
+        <p>This is your dashboard. What would you like to do?</p>
+
+        <ul>
+            <li><a href="#">🔐 Generate a Password</a></li>
+            <li><a href="#">💾 Save a Password</a></li>
+            <li><a href="#">📄 View Saved Passwords</a></li>
+            <li><a href="logout.php">🚪 Logout</a></li>
+        </ul>
+    </div>
 </body>
 </html>
